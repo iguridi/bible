b56ac3a0c323176f0c7d485fa988c1aa4cf61557
iguridi <iguridi@users.noreply.github.com>
2026-07-19 23:52:01 -0700

    Make subtitle and book name flow as single inline phrase
    Changed .mt2, .mt3, .mt on chapter pages to display: inline with
    consistent styling (1rem, weight 500, subtext0 color). The three divs
    now read as one phrase: "The First Book of Moses, Commonly Called
    Genesis". Left-aligned with the text column and ☰ icon.

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const OFFLINE_VERSION = 55;
+const OFFLINE_VERSION = 56;
 const ARCHIVE_CACHE = `archive_v${OFFLINE_VERSION}`;
 const STANDALONE_CACHE = `standalone_v${OFFLINE_VERSION}`;
 
