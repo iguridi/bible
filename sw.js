331653505bc5aaba66d2a8bc29442a09074b060c
iguridi <iguridi@users.noreply.github.com>
2026-07-19 23:33:37 -0700

    Center chapter number grid on book TOC pages

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const OFFLINE_VERSION = 51;
+const OFFLINE_VERSION = 52;
 const ARCHIVE_CACHE = `archive_v${OFFLINE_VERSION}`;
 const STANDALONE_CACHE = `standalone_v${OFFLINE_VERSION}`;
 
